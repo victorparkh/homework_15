38e60d59fd3f5628025d5c5926739a5e9df0bdee
victorparkh <victor.parkh@gmail.com>
2021-02-23 12:18:28 +0200

    first commit

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.adaptive__icon').click(function() {
+        $('.adaptive__icon').toggleClass('open-menu');
+        $('.menu').toggleClass('open-menu');
+    });
+});
